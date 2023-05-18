419628cc8c5876cc6f6a76a6bee8ec7588fae069
LoayEssawy <loay.elessawy@gmail.com>
2023-05-18 08:05:55 +0300

    JS New Update

--- S_CSS_01 Create Top Songs Page/javaSvript.js
+++ S_CSS_01 Create Top Songs Page/javaSvript.js
@@ -1,9 +1,9 @@
 let btnSelect= document.getElementById("showBtn")
 let listShow= document.getElementById("listContainer")
 
-
 btnSelect.addEventListener("click", function(){
     listShow.style.display="flex";
+    btnSelect.style.display="none";
 })
 
 
