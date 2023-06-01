21bde56a9855205f5bebd5f874213b0c3e24d3d7
LoayEssawy <loay.elessawy@gmail.com>
2023-06-01 03:41:33 +0300

    JS3_dpate_1

--- S_JS_03 To-do app edit task inline/New.js
+++ S_JS_03 To-do app edit task inline/New.js
@@ -78,6 +78,7 @@ function createNew(taskName) {
 
             taskList[currentIndex] = newTask;
             document.getElementById(`subBtn`).innerHTML = "Submit"
+            webStatus = "new";
 
         }
         displayProducts()
